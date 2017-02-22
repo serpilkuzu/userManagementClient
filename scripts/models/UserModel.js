d6aa7e6840a67cb4d06878e074fff14f9c328b4b
serpilkuzu <serpil.kuzu@nevalabs.com>
2017-02-22 09:33:06 +0300

    US-6 Added validation to UserModel Fill the form on edit page with selected user information Make validation when creating and editing user

--- scripts/models/UserModel.js
+++ scripts/models/UserModel.js
@@ -1,15 +1,22 @@
 define([
   'backbone'
 ], function(Backbone) {
-  var UserModel = Backbone.Model.extend({
-    defaults: {
-      id: -1,
-      name: "1",
-      surname: "2",
-      age: 2,
-      gender: 0,
-      email: "sample@gmail.com"
-    }
-  });
-  return UserModel;
+    var UserModel = Backbone.Model.extend({
+        urlRoot:  "http://localhost:8080/users",
+        validate: function (attrs) {
+
+            var email_filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+            if(!attrs.name || attrs.name == '') {
+                return "User name should be defined!";
+            } else if(!attrs.surname || attrs.surname == '') {
+                return "User surname should be defined!";
+            } else if(!email_filter.test(attrs.email)) {
+                return "User email is not valid!";
+            } else if(!attrs.age || attrs.age < 0) {
+                return "Age is not valid!";
+            }
+        }
+    });
+    return UserModel;
 });
--- scripts/views/CreateUserView.js
+++ scripts/views/CreateUserView.js
@@ -3,21 +3,49 @@
  */
 define([
     'backbone',
-    'text!templates/createUser.html'
-], function(Backbone, CreateUserTemplate) {
+    'text!templates/createUser.html',
+    'models/UserModel'
+], function(Backbone, CreateUserTemplate, UserModel) {
 
     var createUserView = Backbone.View.extend({
 
         template : _.template(CreateUserTemplate),
 
         initialize: function () {
-            console.log("initializing");
+            console.log("CreateUserView is being initialized");
             this.render();
         },
 
         render: function () {
             this.$el.html(this.template);
             return this;
+        },
+
+        events: {
+            'click #createButton': 'createUser'
+        },
+
+        createUser: function () {
+            var options = {
+                error: function (model, error) {
+                    alert(error);
+                }
+            };
+            var user = new UserModel ({
+                name: $('#firstName').val(),
+                surname: $('#surname').val(),
+                age: parseInt($('#age').val()),
+                email: $('#email').val()
+            });
+
+            if (user.isValid()) {
+                user.save();
+                alert("User is created successfully.");
+                Backbone.history.navigate("list", {trigger: true});
+            } else {
+                console.log("User is not valid");
+                user.save(user, options);
+            }
         }
 
     });
